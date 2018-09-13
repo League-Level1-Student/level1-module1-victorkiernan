47160e1de964675d7732a6b6d88a9e315ef141de
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-09-12 20:00:28 -0700

    sea creature- in progress race car complete backpack in progress

---
+++ src/CatRunner.java
@@ -0,0 +1,9 @@
+
+public class CatRunner {
+public static void main(String[] args) {
+	Cat cat= new Cat("Jeff");
+cat.printName();
+for(int i=0;i<9 ;i++)
+cat.kill();
+}
+}
--- src/RaceCarRunner.java
+++ src/RaceCarRunner.java
@@ -5,14 +5,17 @@ public class RaceCarRunner {
 		/* Do the following things without changing the RaceCar class */
 		
 		// 1. Create a RaceCar and place it in 5th position
-	
+	RaceCar car = new RaceCar("Mustang",5);
 		// 2. Print the RaceCar's position in the race
-
+car.getPositionInRace();
 		// 3. Crash the RaceCar
-		
+		car.crash();
 		// 4. If the car is damaged. Bring it in for a pit stop.
-
+if (car.damaged) {
+	car.pit();
+}
 		// 5. Help the car move into first place.
-
+car.getPositionInRace();
+car.overtake();car.overtake();car.overtake();car.overtake();car.overtake();car.overtake();car.overtake();
 	}
 }
